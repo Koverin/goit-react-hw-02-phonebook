b01bf77b54a8acd4b790f4779a52c1d67d306600
Koverin <110396064+Koverin@users.noreply.github.com>
2023-02-24 17:09:53 +0200

    add new file

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const ContactList = ({ contacts, onDeletedContact }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <p>
+            {name}: {number}
+          </p>
+          <button type="button" onClick={() => onDeletedContact(id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
